15558c2ba953c5533cec57d3078bf16e3ea540d3
NyeUsr <git.h2rt7@simplelogin.co>
2023-03-17 14:57:43 -0500

    Testing...

--- Specific/scriptlets.js
+++ Specific/scriptlets.js
@@ -1,17 +1,8 @@
-(function() {
-    // >>>> start of private namespace
-    'use strict';
-
-
     /// wpadmanager.js
     /// alias wpam.js
     // taken from https://js.wpadmngr.com/static/adManager.js
     // prevent connection to their servers, instead serve the
     // script locally to prevent breakage
-    !function(e){var o=e.document.createElement("script"),t="https://js.wpadmngr.com/static/adManager";"noModule"in o?(o.src="".concat(t,".m.js"),o.async=!0):(o.src="".concat(t,".l.js"),o.defer=!0),["LighthouseGooglebot","Baiduspider","Bingbot","DuckDuckBot","Yahoo! Slurp","facebookexternalhit","YandexBot","ia_archiver","R6_FeedFetcher","NetcraftSurveyAgent","Sogou web spider","PrintfulBot","Twitterbot","APIs-Google","AdsBot-Google-Mobile","AdsBot-Google","Mediapartners-Google","Googlebot-Image","Googlebot","Googlebot-News","Googlebot-Video","AdsBot-Google-Mobile-Apps","FeedFetcher-Google","Google-Read-Aloud","DuplexWeb-Google","googleweblight","Storebot-Google","YandexAdNet","YandexAccessibilityBot","YandexBlogs","YandexBot","YandexCalendar","YandexDirect","YandexDirectDyn","YaDirectFetcher","YandexImages","YandexImageResizer","YandexMobileBot","YandexMarket","YandexMedia","YandexMetrika","YandexMobileScreenShotBot","YandexNews","YandexOntoDB","YandexOntoDBAPI","YandexPagechecker","YandexPartner","YandexRCA","YandexSitelinks","YandexVertis","YandexVerticals","YandexWebmaster","YandexScreenshotBot"].some((function(e){return-1!==navigator.userAgent.toLowerCase().indexOf(e.toLowerCase())}))||e.document.head.appendChild(o)}(window);
-
-
-    // These lines below are skipped by the resource parser.
-    // <<<< end of private namespace
+    (() => {
+    var o=e.document.createElement("script"),t="https://js.wpadmngr.com/static/adManager";"noModule"in o?(o.src="".concat(t,".m.js"),o.async=!0):(o.src="".concat(t,".l.js"),o.defer=!0),["LighthouseGooglebot","Baiduspider","Bingbot","DuckDuckBot","Yahoo! Slurp","facebookexternalhit","YandexBot","ia_archiver","R6_FeedFetcher","NetcraftSurveyAgent","Sogou web spider","PrintfulBot","Twitterbot","APIs-Google","AdsBot-Google-Mobile","AdsBot-Google","Mediapartners-Google","Googlebot-Image","Googlebot","Googlebot-News","Googlebot-Video","AdsBot-Google-Mobile-Apps","FeedFetcher-Google","Google-Read-Aloud","DuplexWeb-Google","googleweblight","Storebot-Google","YandexAdNet","YandexAccessibilityBot","YandexBlogs","YandexBot","YandexCalendar","YandexDirect","YandexDirectDyn","YaDirectFetcher","YandexImages","YandexImageResizer","YandexMobileBot","YandexMarket","YandexMedia","YandexMetrika","YandexMobileScreenShotBot","YandexNews","YandexOntoDB","YandexOntoDBAPI","YandexPagechecker","YandexPartner","YandexRCA","YandexSitelinks","YandexVertis","YandexVerticals","YandexWebmaster","YandexScreenshotBot"].some((function(e){return-1!==navigator.userAgent.toLowerCase().indexOf(e.toLowerCase())}))||e.document.head.appendChild(o)
     })();
-    
